42e8d5b862b0e9414c1df35d979bf908bfce0db0
vaghasiya pinal <130918144+vaghasiyapinal@users.noreply.github.com>
2024-01-01 20:26:11 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import{BrowserRouter,Route,Routes} from "react-router-dom";
 import {Home}from"./pages/Home";
-//import {About}from"./pages/About";
-//import {Contact}from"./pages/Contact";
+import {About}from"./pages/About";
+import {Contact}from"./pages/Contact";
 //import {Service}from"./pages/Service";
 //import {Register}from"./pages/Register";
 //import {Login}from"./pages/Login";
@@ -13,6 +13,12 @@ const App = ()=>{
   <Navbar/>
   <Routes>
     <Route path="/" element={<Home/>}/>
+    <Route path="/about" element={<About/>}/>
+    <Route path="/contact" element={<Contact/>}/>
+    <Route path="/service" element={<Service/>}/>
+    <Route path="/register" element={<Register/>}/>
+    <Route path="/login" element={<Login/>}/>
+    <Route path="/navbar" element={<Navbar/>}/>
     
   </Routes>
   </BrowserRouter>)
